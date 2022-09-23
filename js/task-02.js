058a13f5cdc44ff32007df103a678e1b70d9d85d
katya <92817792+Katya2211@users.noreply.github.com>
2022-09-23 11:38:21 +0300

    HW6

---
+++ js/task-02.js
@@ -0,0 +1,24 @@
+const ingredients = [
+    "Potatoes",
+    "Mushrooms",
+    "Garlic",
+    "Tomatos",
+    "Herbs",
+    "Condiments",
+  ];
+  
+  const listRef = document.querySelector("#ingredients");
+  
+  const arr = [];
+  
+  const separateIngredients = ingredients.map((item) => {
+    const itemRef = document.createElement("li");
+  
+    itemRef.textContent = item;
+    itemRef.classList.add("item");
+  
+    arr.push(itemRef);
+  });
+  
+  listRef.append(...arr);
+  console.log(listRef);
---
+++ js/task-09.js
@@ -0,0 +1,16 @@
+const refs = {
+    color: document.querySelector(".color"),
+    changeColorBtn: document.querySelector(".change-color"),
+    body: document.querySelector("body"),
+  };
+  
+  refs.changeColorBtn.addEventListener("click", () => {
+    refs.body.classList.add("body");
+    const bodyRef = document.querySelector(".body");
+    refs.body.style.backgroundColor = getRandomHexColor();
+    refs.color.textContent = getRandomHexColor();
+  });
+  
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
